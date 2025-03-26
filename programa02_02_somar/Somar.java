ba0711ab934df954b28854ac39c7f93d81a29bab
gubartz <gubartz@gmail.com>
2025-03-26 12:02:12 -0300

    Programa Somar

---
+++ programa02_02_somar/Somar.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Somar {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número:");
+        double num1 = scanner.nextDouble();
+        
+        System.out.println("Digite o segundo número:");
+        double num2 = scanner.nextDouble();
+
+        // O método nextDouble() lê apenas o número, mas não consome a quebra de linha (\n) que o usuário digitou ao pressionar "Enter".
+        scanner.nextLine();
+
+        double result = 0;
+        result = somar(num1, num2);
+        
+        System.out.println("O resultado é: " + result);
+    }
+
+    public static double somar(double num1, double num2){
+        return num1 + num2;
+    }
+
+}
